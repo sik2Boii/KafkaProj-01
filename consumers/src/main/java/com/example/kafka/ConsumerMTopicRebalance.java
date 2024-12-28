5387a234b0be8b0b29acfc26148e6620ff3a5e5b
Junsik <junsik213@naver.com>
2024-12-29 04:04:51 +0900

    Consumer에서 여러 개 Topic 읽기

---
+++ consumers/src/main/java/com/example/kafka/ConsumerMTopicRebalance.java
@@ -0,0 +1,61 @@
+package com.example.kafka;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerMTopicRebalance {
+
+    public static final Logger logger = LoggerFactory.getLogger(ConsumerMTopicRebalance.class.getName());
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-mtopic");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
+        kafkaConsumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2")); // 2개의 토픽을 구독
+
+        Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                logger.info("main program starts to exit by calling wakeup");
+                kafkaConsumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord record : consumerRecords) {
+                    logger.info("topic: {}, record key: {},  partition: {}, record offset: {} record value: {}",
+                            record.topic() , record.key(), record.partition(), record.offset(), record.value());
+                }
+            }
+        } catch (WakeupException e) {
+            logger.error("wakeup exception has been called");
+        } finally {
+            logger.info("finally consumer is closing");
+            kafkaConsumer.close();
+        }
+    }
+}
